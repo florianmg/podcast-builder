1ba13098a320728cff230dcd80bdfc6fadc49342
Florian <mesagouiffes@gmail.com>
2021-04-15 08:41:25 +0200

    feat(authentication) create Navbar component

---
+++ components/Navbar.component.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar: React.FC = () => {
+  return (
+    <ul>
+      <li>S'inscrire</li>
+      <li>Se connecter</li>
+    </ul>
+  )
+}
+
+export default Navbar
